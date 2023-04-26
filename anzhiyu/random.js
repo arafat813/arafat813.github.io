61cb8f1882b48095e3646eaa8f20f9e46f2bdc85
“arafat813” <1017012106@qq.com>
2023-04-26 17:23:02 +0800

    Site updated: 2023-04-26 17:23:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/2d900bbb.html","posts/4db0c680.html","posts/888898f6.html","posts/90039cb8.html","posts/b0b32b9.html","posts/f55bc382.html","posts/9b47c56e.html","posts/f4a69292.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/f50a3637.html","posts/4db0c680.html","posts/888898f6.html","posts/90039cb8.html","posts/2d900bbb.html","posts/b0b32b9.html","posts/f55bc382.html","posts/9b47c56e.html","posts/f4a69292.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
